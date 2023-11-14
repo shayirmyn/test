5edfa0bcf4f0a39628559854bec26c322b1b416d
Shayirbek Abdymomunov <legionera803@gmail.com>
2023-11-15 05:45:48 +0600

    redone

--- src/App.js
+++ src/App.js
@@ -15,24 +15,25 @@ const App = () => {
         getAllPokemonst();
     }, []);
 
-    console.log(pokemons);
-
     return (
-      <>
-          <div className="mainContainer d-flex flex-wrap w-75">
-              {
-                  pokemons.map((every) => (
-                      <div className="card w-25" key={every.name}>
-                          <img src={every.url} alt={every.name}/>
-                          <div className="card-body">
-                              <h5 className="card-title">{every.name}</h5>
-                          </div>
-                      </div>
-                  ))
-              }
-          </div>
-      </>
-  )
+        <>
+            <div className="mainContainer d-flex flex-wrap w-75">
+                {
+                    pokemons.map((every, index) => (
+                        <div className="card w-25"
+                             key={every.name}
+                        >
+                            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
+                                 alt={every.name} className="w-50"/>
+                            <div className="card-body">
+                                <h5 className="card-title">{every.name}</h5>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
 };
 
 export default App;
